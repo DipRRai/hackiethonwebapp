50a259cce1c8e7f0181738007cc5e21e9cbdf0e5
Fengax <857514.leofeng@gmail.com>
2021-04-03 03:05:26 +1100

    added near real-time client web communication

--- C# App/HackiethonProject/HackiethonProject/Form1.cs
+++ C# App/HackiethonProject/HackiethonProject/Form1.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +19,6 @@ namespace HackiethonProject
     public partial class Form1 : Form
     {
         private static readonly HttpClient client = new HttpClient();
-
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +26,7 @@ namespace HackiethonProject
 
         private async void btnSubmit_Click(object sender, EventArgs e)
         {
-
+            
             var values = new Dictionary<string, string>
             {
                 {"hours",  txtData.Text}
@@ -32,7 +34,6 @@ namespace HackiethonProject
             var content = new FormUrlEncodedContent(values);
             var response = await client.PostAsync("http://127.0.0.1:5000/stats/", content);
             var responseString = await response.Content.ReadAsStringAsync();
-            MessageBox.Show(responseString);
         }
     }
 }
--- flaskapp.py
+++ flaskapp.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///input.db'
 db = SQLAlchemy(app)
 
 g_hours = "default"
+isChanged = False
 
 @app.route("/", methods=['POST'])
 def home():
     return render_template("home.html")
 
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    global isChanged
+    # GET request
+    if request.method == 'GET':
+        if isChanged:
+            message = {'changed':'yes'}
+            isChanged = False
+        else:
+            message = {'changed':'no'}
+        return jsonify(message)  # serialize and use JSON headers
+
 @app.route('/stats/', methods=["POST", "GET"])
 def display():
     global g_hours
+    global isChanged
     if request.method == "POST":
         g_hours = request.form["hours"]
+        isChanged = True
         return redirect("/stats/")
-    return render_template("statsOverview.html", hours=g_hours)
+    else:
+        return render_template("statsOverview.html", hours=g_hours)
 
 @app.route('/stats/<hours>', methods=['POST','GET'])
 def stats(hours):
