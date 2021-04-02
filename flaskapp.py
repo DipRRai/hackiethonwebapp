805dc0b05006847cfd0a039ff58f407bdb701770
superderped <superderped@gmail.com>
2021-04-03 02:36:19 +1100

    graphing javascript added

--- flaskapp.py
+++ flaskapp.py
@@ -6,7 +6,7 @@ db = SQLAlchemy(app)
 
 g_hours = "default"
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST', 'GET'])
 def home():
     return render_template("home.html")
 
@@ -18,9 +18,30 @@ def display():
         return redirect("/stats/")
     return render_template("statsOverview.html", hours=g_hours)
 
-@app.route('/stats/<hours>', methods=['POST','GET'])
-def stats(hours):
-    return render_template("statsOverview.html", hours = hours)
+@app.route('/stats/<hours>/<days>', methods=['POST','GET'])
+def stats(hours, days):
+    temp = ""
+    ylabl = []
+    for i in hours:
+        if i != ('~'):
+            temp = temp + i
+        elif i == ('~'):
+            ylabl.append(float(temp))
+            temp = ""
+    temp = ""
+    xlabl = []
+    for i in days:
+        if i != ('~'):
+            if i == ':':
+                temp = temp + "/"
+            else:
+                temp = temp + i
+        elif i == ('~'):
+            xlabl.append(str(temp))
+            temp = ""
+    print(xlabl)
+    print(ylabl)
+    return render_template("statsOverview.html", xlabl = xlabl, ylabl = ylabl)
 
 
 if __name__ == "__main__":
